0bd3c8d4747e60ad11da0a64a5b1132e44d542c9
lucianoojeda36 <lucianoojeda36@gmail.com>
2024-10-03 11:01:54 -0300

    update

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,6 +19,9 @@ from dotenv import load_dotenv
 # Carga las variables del archivo .env
 load_dotenv()
 
+PORT = os.getenv('SERVICE_PORT', '8000')  # Valor por defecto es 8000
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
